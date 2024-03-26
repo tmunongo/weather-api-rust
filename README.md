6154b0d2aae38fa110d45ce32f33bb157ad50f13
Tawanda <tawanda@zapiet.com>
2024-03-26 09:11:57 +0200

    Working reqwests to the Weather API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Details
+
+Repo for a tutorial that I'm working on. Will implement caching with Redis.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::env;
+
 use axum::{
     extract::Path, http::StatusCode, routing::get, Router
 };
+use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
-// use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() {
@@ -23,5 +25,41 @@ async fn index() -> &'static str {
 }
 
 async fn city_weather(Path(city): Path<String>) -> String {
-    format!("Weather for {}", city)
+    let api_key = env::var("WEATHER_API_KEY").expect("WEATHER_API_KEY must be set");
+
+    let request_string = format!("http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no", api_key, city);
+
+    let response = reqwest::get(request_string)
+    .await
+    .unwrap();
+
+    let body: WeatherResponse = response.json::<WeatherResponse>().await.unwrap();
+
+    format!("Weather for {:?}", body)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WeatherResponse {
+    location: Location,
+    current: Current,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Location {
+    name: String,
+    region: String,
+    country: String,
+    lat: f64,
+    lon: f64,
+    tz_id: String,
+    localtime_epoch: i64,
+    localtime: String
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Current {
+    last_updated_epoch: i64,
+    last_updated: String,
+    temp_c: f32,
+    temp_f: f32,
 }
