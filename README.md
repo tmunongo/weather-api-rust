28d39cc92d0c02b34f4b66127ce86a305764b407
Tawanda <ejmunongo@gmail.com>
2024-04-04 08:51:32 +0200

    Final

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Details
 
-Repo for a tutorial that I'm working on. Will implement caching with Redis.
+Repo for a tutorial that I'm working on. We build a weather API, using Redis to cache results from the API to:
+
+1. Save up on API credits
+2. Make our application faster and reduce response times
 
 ## Stack
 
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use axum::{
     extract::{Path, State},
     http::StatusCode,
     routing::get,
-    Router,
+    Json, Router,
 };
 use bb8::Pool;
 use bb8_redis::RedisConnectionManager;
 use dotenvy::dotenv;
-use redis::AsyncCommands;
+use redis::{AsyncCommands, FromRedisValue, RedisResult};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
@@ -17,8 +17,10 @@ use tokio::net::TcpListener;
 async fn main() {
     dotenv().expect("Failed to load .env file");
 
+    let redis_url = env::var("REDIS_URL").expect("REDIS URL must be set!");
+
     tracing::debug!("connecting to redis");
-    let manager = RedisConnectionManager::new("redis://127.0.0.1").unwrap();
+    let manager = RedisConnectionManager::new(redis_url).unwrap();
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
 
     {
@@ -47,27 +49,26 @@ async fn index() -> &'static str {
 
 type ConnectionPool = Pool<RedisConnectionManager>;
 
-async fn city_weather(State(pool): State<ConnectionPool>, Path(city): Path<String>) -> String {
+async fn city_weather(
+    State(pool): State<ConnectionPool>,
+    Path(city): Path<String>,
+) -> Result<Json<WeatherResponse>, String> {
     let api_key = env::var("WEATHER_API_KEY").expect("WEATHER_API_KEY must be set!");
 
     // check redis for the weather data
     let mut conn = pool.get().await.unwrap();
-    let cached_weather = conn
-        .get::<String, Option<String>>(city.as_str().to_owned())
+    let cached_weather: Option<WeatherResponse> = conn
+        .get::<String, Option<WeatherResponse>>(city.as_str().to_owned())
         .await
         .unwrap();
 
     let body: WeatherResponse;
 
     if cached_weather.is_some() {
-        return format!(
-            "Weather for {} was retrieved from cache: {:?}",
-            city,
-            serde_json::from_str::<WeatherResponse>(&cached_weather.unwrap()).unwrap(),
-        );
+        return Ok(Json(cached_weather.unwrap()));
     } else {
         let request_string = format!(
-            "http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
+            "https://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
             api_key, city
         );
 
@@ -75,8 +76,8 @@ async fn city_weather(State(pool): State<ConnectionPool>, Path(city): Path<Strin
 
         body = response.json::<WeatherResponse>().await.unwrap();
 
-        let cached_body = conn
-            .set_ex::<&str, String, Option<String>>(
+        let cached_body: Option<WeatherResponse> = conn
+            .set_ex(
                 &body.location.name.to_ascii_lowercase(),
                 serde_json::to_string(&body).unwrap(),
                 3600,
@@ -85,25 +86,38 @@ async fn city_weather(State(pool): State<ConnectionPool>, Path(city): Path<Strin
             .unwrap();
 
         match cached_body {
-            Some(cached) => {                
-                return format!(
-                    "Weather for {} was cached as {:?}",
-                    body.location.name,
-                    cached
-                )
-            }
-            None => return format!("Weather for {} was not cached", body.location.name),
+            Some(_) => Ok(Json(body.clone())),
+            None => Err(format!("Weather for {} was not cached", body.location.name)),
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct WeatherResponse {
     location: Location,
     current: Current,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+impl FromRedisValue for WeatherResponse {
+    fn from_redis_value(v: &redis::Value) -> RedisResult<Self> {
+        match v {
+            redis::Value::Data(data) => {
+                // Assuming the data stored in Redis is JSON representing a WeatherResponse
+                let json_str = std::str::from_utf8(data).expect("Invalid UTF-8 data in Redis");
+                let weather_response: WeatherResponse =
+                    serde_json::from_str(json_str).expect("Failed to deserialize JSON");
+
+                Ok(weather_response)
+            }
+            _ => Err(redis::RedisError::from((
+                redis::ErrorKind::TypeError,
+                "Invalid Redis value type for WeatherResponse",
+            ))),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
 struct Location {
     name: String,
     region: String,
@@ -115,7 +129,7 @@ struct Location {
     localtime: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 struct Current {
     last_updated_epoch: i64,
     last_updated: String,
